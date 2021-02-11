49710781693b094e19301aa6d4fd456c72fc4fc9
Azizbek <arasulov0066@gmail.com>
2021-02-11 11:29:48 +0500

    start

---
+++ js/main.js
@@ -0,0 +1,15 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+    navigation:true,
+    scrollbar:true,
+    scrollOverflow:true,
+    loopBottom:true,
+    loopTop:true,
+    scrollingSpeed:500,
+    fadingEffect:true
+});
+
+//methods
+// fullpage_api.setAllowScrolling(false);
